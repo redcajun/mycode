e7921329106d14ca65872364b9b8e123c8175446
Chris Barnett <redcajun@users.noreply.github.com>
2021-08-23 22:20:37 +0000

    About complex lists

---
+++ advlist/animal-list01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# create a list of five animals with three letters in their name
+animal_list = ['fox', 'cat', 'dog', 'bee', 'ant']
+print(animal_list)
+
---
+++ advlist/animallist.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# create a list with five animals with three letter names
+animal_list = ['dog', 'cat', 'fox', 'bee', 'ant']
+print(animal_list)
+
